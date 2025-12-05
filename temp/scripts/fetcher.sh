3bef57ec8b89eae09545498abe44b50cc15a979f
Uri Neri <uneri at t lbl.gov>
2025-12-04 17:17:26 -0800

    remove temp file, add temporary tree utils (might replace later)

--- temp/scripts/fetcher.sh
+++
@@ -1,9 +0,0 @@
-<REPO_PATH>/.pixi/envs/dev/bin/datasets download genome taxon 131567 \
-    --assembly-source GenBank \
-    --include genome,gff3 \
-    --filename cellular_organisms_genbank.zip \
-    --dehydrated \
-    --fast-zip-validation \
-    --assembly-version latest \
-    --no-progressbar \
-    
